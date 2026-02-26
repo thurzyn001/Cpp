77e7fc98c401ce47df1c81383e28040a7c5ab169
Arthur Furtado <zynthur@gmail.com>
2026-02-26 20:37:26 -0300

    Add quadrados & repeticao; update OlaMundo.exe
    Add two new example C++ programs and their Windows binaries: quadrados.cpp (prints squares for 1..10) and repeticao.cpp (iterates an int array and prints elements at indices 1 and 2). Also update the OlaMundo.exe binary. These are simple loop examples for demonstration/testing.

---
+++ quadrados.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    for (int i = 1; i < 11; i++) {
+        cout << "Quadrado de: " << i << " = " << i * i << endl;
+    }
+
+return 0;
+
+}
---
+++ repeticao.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int numeros[3] = {10, 20, 30};
+
+    for (int i = 1; i < 3; i++) {
+        cout << numeros[i] << endl;
+    }
+
+}
